182a1de2bf8895952cc707c3eb4e613c15289012
thewaifuai <thewaifuai@users.noreply.github.com>
2022-01-31 07:30:52 +0000

    commit

--- waifuai/waifuai.py
+++ waifuai/waifuai.py
@@ -19,13 +19,13 @@ def get_response(X_RAPIDAPI_KEY: str, user_id: str = "", message: str = "", from
     parsed_preset_mode = urllib.parse.quote(preset_mode)
     THE_PAYLOAD = f"message={parsed_message}&user_id={parsed_user_id}&from_name={parsed_from_name}&to_name={parsed_to_name}&situation={parsed_situation}&translate_to={parsed_translate_to}&preset_mode={parsed_preset_mode}"
 
-    url = "https://waifu-ai.p.rapidapi.com/path"
+    url = "https://waifu.p.rapidapi.com/path"
 
     payload = THE_PAYLOAD
     headers = {
         'content-type': "application/x-www-form-urlencoded",
         'x-rapidapi-key': X_RAPIDAPI_KEY,
-        'x-rapidapi-host': "waifu-ai.p.rapidapi.com"
+        'x-rapidapi-host': "waifu.p.rapidapi.com"
     }
 
     response = requests.request("POST", url, data=payload, headers=headers)
